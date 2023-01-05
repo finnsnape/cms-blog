6cdc8746c6a0f5b5e4ff87a6e93658419f29a901
finnsnape <snapefinn@gmail.com>
2023-01-05 21:29:35 +0000

    Added skeleton header/footer

--- svelte-kit/src/routes/+layout.svelte
+++ svelte-kit/src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
   import "$lib/scss/app.scss";
+  import Header from "$lib/components/general/Header.svelte";
+  import Footer from "$lib/components/general/Footer.svelte";
 </script>
 
+<Header />
 <slot />
+<Footer />
