04adc5cab842c91a504880b52f6d7aac2b07b55b
finnsnape <snapefinn@gmail.com>
2023-02-06 17:38:04 +0000

    Added basic blog fetching based on slug

---
+++ app/src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+  /** @type {import('./$types').PageData} */  export let data;
+console.log(data);
+</script>
+
+<h1>{data.post.title}</h1>
+<div>{@html data.post.body}</div>
---
+++ app/src/routes/blog/[slug]/+page.ts
@@ -0,0 +1,25 @@
+/** @type {import('./$types').PageLoad} */
+export async function load({ params }) {
+    console.log(params.slug);
+    const response = await fetch("http://localhost:8055/graphql", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            query: `
+                query {
+	               post: post_by_id(id: "${params.slug}") {
+                      id
+		              title
+                      body
+	               }
+                }
+            `
+        })
+    });
+    if (response.ok) {
+        const post = await response.json();
+        return post.data;
+    }
+}
