878cd46cb173b25b037c903a127b677a279963cf
finnsnape <snapefinn@gmail.com>
2023-02-06 20:52:32 +0000

    Now loading and parsing basic data on blog post page

--- app/src/routes/blog/[slug]/+page.svelte
+++ app/src/routes/blog/[slug]/+page.svelte
@@ -1,7 +1,38 @@
-<script>
-  /** @type {import('./$types').PageData} */  export let data;
+<script lang="ts">
+    /** @type {import('./$types').PageData} */  export let data;
+    function addClasses(body: string): string {
+        const classMap = {
+            "h2": "heading-2",
+            "h3": "heading-3"
+        }
+        for (const [key, value] of Object.entries(classMap)) {
+            body = body.replaceAll(`<${key}>`,`<${key} class="${value}">`);
+        }
+        return body;
+    }
+
+    function parseDate(date: string): string {
+        const dateObject: Date = new Date(date);
+        return dateObject.toLocaleDateString("en-GB", { day: "numeric", month: "long", year: "numeric" })
+    }
+
+    data.post.body = addClasses(data.post.body);
+    data.post.date_created = parseDate(data.post.date_created);
 </script>
 
-<h1>{data.post.title}</h1>
-<img src="http://localhost:8055/assets/{data.post.banner}" alt="">
-<div>{@html data.post.body}</div>
+<style lang="scss">
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+        align-self: center;
+    }
+</style>
+
+<div class="wrapper stack-recursive">
+    <img src="http://localhost:8055/assets/{data.post.banner}" alt="">
+    <h1 class="heading-1 ch-xs">{data.post.title}</h1>
+    <p>{data.post.date_created}</p>
+    <div class="ch-xl paragraph-2">
+        {@html data.post.body}
+    </div>
+</div>
--- app/src/routes/blog/[slug]/+page.ts
+++ app/src/routes/blog/[slug]/+page.ts
@@ -1,5 +1,5 @@
 /** @type {import('./$types').PageLoad} */
-export async function load({ params }) {
+export async function load({ fetch, params }) {
     const response = await fetch("http://localhost:8055/graphql", {
         method: "POST",
         headers: {
