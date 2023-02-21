78fd91d25c578dfea8b3e3ab32402e0bf3add63a
finnsnape <snapefinn@gmail.com>
2023-02-21 15:30:52 +0000

    Added basic header with nav

---
+++ app/src/lib/components/general/Header.svelte
@@ -0,0 +1,25 @@
+<style lang="scss">
+  .wrapper {
+    display: flex;
+    justify-content: space-between;
+    margin: $size-020 $space-xl-3xl;
+  }
+
+  .logo {
+    
+  }
+
+  nav {
+    display: flex;
+    align-items: center;
+    column-gap: $size-040;
+  }
+</style>
+
+<div class="wrapper">
+  <h3 class="logo heading-3">Logo</h3>
+  <nav class="paragraph-3">
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+  </nav>
+</div>
