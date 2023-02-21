2f8b2bb57846debf8b9089931e743b963b55b98a
finnsnape <snapefinn@gmail.com>
2023-02-21 15:45:35 +0000

    Styled error page

--- app/src/routes/+error.svelte
+++ app/src/routes/+error.svelte
@@ -2,4 +2,19 @@
     import { page } from '$app/stores';
 </script>
 
-<h1>{$page.error.message}</h1>
+<style lang="scss">
+  .wrapper {
+    display: flex;
+    margin: $space-xl-3xl 0;
+    align-items: center;
+    justify-content: center;
+  }
+  h1 {
+
+  }
+</style>
+
+<div class="wrapper">
+  <h1>{$page.error.message}</h1>
+</div>
+
