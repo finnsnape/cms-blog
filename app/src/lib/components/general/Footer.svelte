67d8bfff126cf63e4229181800861715cac067cf
finnsnape <snapefinn@gmail.com>
2023-02-21 15:45:08 +0000

    Added basic footer with placeholder links

---
+++ app/src/lib/components/general/Footer.svelte
@@ -0,0 +1,25 @@
+<style lang="scss">
+  .wrapper {
+    display: flex;
+    justify-content: center;
+    margin: $size-020 $space-xl-3xl;
+  }
+
+  nav {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+</style>
+
+<div class="wrapper">
+  <footer>
+    <nav class="paragraph-3 stack">
+      <a href="/">Home</a>
+      <a href="/blog">Blog</a>
+      <a href="/privacy">Privacy</a>
+      <a href="/accessibility">Accessibility</a>
+    </nav>
+  </footer>
+
+</div>
